269ede00887923e53cc54330c81f68b7ff9176ea
Shinjoku <deora.2@hotmail.com>
2017-07-08 04:54:10 -0300

    + comments

--- survivors/api/routes/survivorsRoutes.js
+++ survivors/api/routes/survivorsRoutes.js
@@ -3,22 +3,26 @@
 module.exports = function(app) {
     var survivors = require('../controllers/survivorsController');
 
-    // Rotas
+    // To list all the survivors or create another one
     app.route('/survivors')
         .get(survivors.listAllSurvivors)
         .post(survivors.createASurvivor);
 
+    // To show the infos about the network
     app.route('/survivors/infos')
         .get(survivors.infos);
+    
+    // To report one survivor
+    app.route('/survivors/report/:survivorId')
+        .put(survivors.reportASurvivor);
 
+    // To detail, update or delete one survivor
     app.route('/survivors/:survivorId')
         .get(survivors.detailASurvivor)
         .put(survivors.updateASurvivor)
         .delete(survivors.deleteASurvivor);
-
-    app.route('/survivors/report/:survivorId')
-        .put(survivors.reportASurvivor);
-
+    
+    // In case of 404
     app.use(function(req, res) {
         res.status(404).send({url: req.originalUrl + ' not found.'});
     });
